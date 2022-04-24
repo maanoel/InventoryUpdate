0a71b35fe5ccf85d2a1f4ab9ced429e87cdfa985
maanoel <manoelvitorbrito@gmail.com>
2022-04-23 21:29:59 -0300

    matrix array c#

---
+++ InventoryUpdate/Iventory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace InventoryUpdate
+{
+  public class Iventory
+  {
+    public dynamic[] Update(dynamic[][] curInv, dynamic[][] newInv)
+    {
+      int newInvOriginalLength = newInv.Length;
+
+      for (int i = 0; i < newInvOriginalLength; i++)
+      {
+        bool bFind = false;
+
+        for (int j = 0; j < newInvOriginalLength; j++)
+        {
+          if (newInv[i][1].Equals(curInv[j][1]))
+          {
+            bFind = true;
+            break;
+          }
+        }
+
+        if (bFind)
+        {
+          Array.Resize(ref curInv, curInv.Length + 1);
+          curInv[newInv.Length] = curInv[i];
+        }
+
+      }
+      return new dynamic[][] { };
+    }
+  }
+}
---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,33 @@
+using InventoryUpdate;
+using Xunit;
+
+namespace TestProject
+{
+  public class UnitTest1
+  {
+    [Fact]
+    public void Should_Return_An_Matrix_Array()
+    {
+      Iventory iventory = new Iventory();
+
+      dynamic[][] curInv = new dynamic[][] {
+        new dynamic[] { 21, "Bowling Ball" },
+        new dynamic[] { 2, "Dirty Sock" },
+        new dynamic[] { 1, "Hair Pin" },
+        new dynamic[] { 5, "Microphone" }
+      };
+
+      dynamic[][] newInv = new dynamic[][] {
+        new dynamic[] { 2, "Hair Pin" },
+        new dynamic[] { 3, "Half-Eaten Apple" },
+        new dynamic[] { 67, "Bowling Ball" },
+        new dynamic[] { 7, "Toothpaste" }
+      };
+
+      dynamic[] iventoryUpdated = iventory.Update(curInv, newInv);
+
+      Assert.IsType<dynamic[][]>(iventoryUpdated);
+      Assert.NotNull(iventoryUpdated);
+    }
+  }
+}
